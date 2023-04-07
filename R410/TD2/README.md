4f39b94be732a97f74ec07d787b48f5481fbd9bc
Najatsu <104486973+Najatsu@users.noreply.github.com>
2023-04-07 07:57:30 +0200

    Add files via upload

--- R410/TD2/README.md
+++ R410/TD2/README.md
@@ -6,3 +6,9 @@ J'ai ajouté l'écouteur d'évènement sur le body, via le code suivant : **docu
 
 currentTarget pointe vers **l'élément** sur lequel l'EventListener **a été attaché**, alors que target pointe vers **l'élément** à partir duquel l'évènement **s'est produit**.
 Si on l'utilise, tout le body devient rose.
+
+Tout d'abord, je récupère le type de l'élèment à ajouter ainsi que le texte qu'il doit contenir, grâce à **getElementById("ID").value**.
+Ensuite, je crée l'élément en question et je lui ajoute le texte avec **newElement.appendChild(textNode);**, puis j'appelle **insertBefore()**.
+
+J'ai vérifié que ni l'élément sélectionné ni son parent ne contenait l'id **insert-div**.
+
--- R410/TD2/js/td2.js
+++ R410/TD2/js/td2.js
@@ -34,8 +34,8 @@ function Select(event) {
 	let selectedElement = event.target; 
 
 	if (
-	  !selectedElement.classList.contains("insert-div") &&
-	  !selectedElement.parentNode.classList.contains("insert-div")
+	  selectedElement.id != "insert-div" &&
+	  selectedElement.parentNode.id != "insert-div"
 	) {
 	  
 	  let previouslySelectedElement = document.querySelector(".selected2");
@@ -44,13 +44,103 @@ function Select(event) {
 	  }
   
 	  selectedElement.classList.add("selected2");
+	  insertElement(selectedElement);
+	}
+  }
+
+function insertElement(target) {
+	
+	let elementType = document.getElementById("insert-type").value;
+	let elementText = document.getElementById("insert-text").value;
+  
+	let newElement = document.createElement(elementType);
+	let textNode = document.createTextNode(elementText);
+	newElement.appendChild(textNode);
+  
+	target.parentNode.insertBefore(newElement, target);
+}
+
+function search() {
+	const selectElements = document.querySelectorAll('.select');
+	
+	selectElements.forEach(element => {
+  	element.classList.remove('select');
+	});
+
+	const searchTerm = document.getElementById('searchTerm').value;
+	const regex = new RegExp(searchTerm, 'gi');
+	// J'utilise un node iterator pour parcourir les noeuds et non les balises elles mêmes.
+	const iterator = document.createNodeIterator(
+	  document.body,
+	  NodeFilter.SHOW_TEXT,
+	  { acceptNode: function(node) { return NodeFilter.FILTER_ACCEPT; } }
+	);
+  
+	let node;
+	while (node = iterator.nextNode()) {
+	  if (regex.test(node.nodeValue)) {
+		const matches = node.nodeValue.match(regex);
+		matches.forEach(match => {
+		  const span = document.createElement('span');
+		  span.className = 'select';
+		  const startIndex = node.nodeValue.indexOf(match);
+		  const endIndex = startIndex + match.length;
+		  const prefix = node.nodeValue.substring(0, startIndex);
+		  const suffix = node.nodeValue.substring(endIndex);
+		  const highlighted = document.createTextNode(match);
+		  node.parentNode.insertBefore(document.createTextNode(prefix), node);
+		  node.parentNode.insertBefore(span, node);
+		  span.appendChild(highlighted);
+		  node.parentNode.insertBefore(document.createTextNode(suffix), node);
+		  node.parentNode.removeChild(node);
+		});
+	  }
+	}
+  }
+
+function interactiveSearch() {
+	const selectElements = document.querySelectorAll('.select');
+	
+	selectElements.forEach(element => {
+  	element.classList.remove('select');
+	});
+
+	const searchTerm = document.getElementById('InteractsearchTerm').value; 
+	const regex = new RegExp(searchTerm, 'gi');
+	// J'utilise un node iterator pour parcourir les noeuds et non les balises elles mêmes.
+	const iterator = document.createNodeIterator(
+	  document.body,
+	  NodeFilter.SHOW_TEXT,
+	  { acceptNode: function(node) { return NodeFilter.FILTER_ACCEPT; } }
+	);
+  
+	let node;
+	while (node = iterator.nextNode()) {
+	  if (regex.test(node.nodeValue)) {
+		const matches = node.nodeValue.match(regex);
+		matches.forEach(match => {
+		  const span = document.createElement('span');
+		  span.className = 'select';
+		  const startIndex = node.nodeValue.indexOf(match);
+		  const endIndex = startIndex + match.length;
+		  const prefix = node.nodeValue.substring(0, startIndex);
+		  const suffix = node.nodeValue.substring(endIndex);
+		  const highlighted = document.createTextNode(match);
+		  node.parentNode.insertBefore(document.createTextNode(prefix), node);
+		  node.parentNode.insertBefore(span, node);
+		  span.appendChild(highlighted);
+		  node.parentNode.insertBefore(document.createTextNode(suffix), node);
+		  node.parentNode.removeChild(node);
+		});
+	  }
 	}
   }
 
 function onLoad() {
 	console.log( 'Processus de chargement du document terminé…');
-	document.body.addEventListener('click',Select);
-	document.body.addEventListener('click',Select2);
+	//document.body.addEventListener('click',Select);
+	//document.body.addEventListener('click',Select2);
+	document.getElementById('InteractsearchTerm').addEventListener('input',interactiveSearch);
 
 }
 
